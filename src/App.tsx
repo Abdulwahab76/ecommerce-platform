1e5535d0ea5a317f6bd1cc4342babdb9c4f9c88f
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2025-05-13 21:28:37 +0500

    feat: update files & code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/layout/navbar";
 
 const App = () => {
   return (
     <AuthProvider>
+      <Navbar />
       <AppRoutes />
     </AuthProvider>
   );
