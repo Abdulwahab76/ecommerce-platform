c48d887733fd02fb1cc03228d4547ef7c523dcc3
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2025-05-31 00:03:52 +0500

    feat: update product card ui

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,21 +1,26 @@
+import { ArrowRight } from "lucide-react";
 import React from "react";
+import { Link } from "react-router-dom";
+import type { ProductT } from "../services/contentful";
 
-type ProductCardProps = {
-    id: string;
-    name: string;
-    price: number;
-    image: string;
-};
 
-const ProductCard: React.FC<ProductCardProps> = ({ name, price, image }) => {
+const ProductCard: React.FC<ProductT> = ({ name, price, image, slug }) => {
     return (
         <>
-            <div className="rounded-lg p-10 hover:shadow-lg transition-shadow duration-300 ease-in-out  cursor-pointer  ">
-                <div className="bg-gray-100 p-5">
+            <div className="rounded-lg p-10 hover:shadow-xl transition-shadow duration-300 ease-in-out  cursor-pointer shadow-lg  ">
+                <div className="  p-5">
                     <img src={image} alt={name} className="w-full h-48 object-cover rounded-lg" />
                 </div>
-                <h2 className="mt-2 text-lg font-semibold">{name}</h2>
-                <p className="text-gray-500">${price}</p>
+                <h2 className="mt-2 text-2xl capitalize font-semibold">{name}</h2>
+                <div className="flex justify-between items-center">
+                    <p className="text-gray-800 text-lg">${price}</p>
+                    <div className="w-7 h-7 bg-black rounded-full flex justify-center items-center ">
+                        <Link to={`/product/${slug}`} className="text-primary  font-semibold   ">
+                            <ArrowRight className="text-white  " size={14} />
+                        </Link>
+                    </div>
+                </div>
+
             </div>
         </>
     );
