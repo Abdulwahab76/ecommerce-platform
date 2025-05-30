c4f2c4be28ca8aa8432d5f957077462da78cb151
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2025-05-31 00:03:41 +0500

    feat: featured products

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,23 +1,20 @@
-// import { fetchFeaturedProducts } from "../services/productService";
-import type { ProductType } from "../data/mockProducts";
+import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import { fetchProducts, type ProductT } from "../services/contentful";
 
-const FeaturedProducts = ({ type, data }: { type: string, data: ProductType[] }) => {
-    // const [products, setProducts] = useState<any[]>([]);
+const FeaturedProducts = ({ type }: { type: string }) => {
 
-    // useEffect(() => {
-    //     const loadProducts = async () => {
-    //         const data = await fetchFeaturedProducts(type);
-    //         setProducts(data);
-    //     };
-    //     loadProducts();
-    // }, [type]);
+    const [products, setProducts] = useState<ProductT[]>([]);
+
+    useEffect(() => {
+        fetchProducts().then(setProducts);
+    }, []);
 
     return (
         <div className="flex justify-center flex-col items-center py-10 w-full">
             <h2 className="text-2xl font-semibold mb-4 font-integral">{type}</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {data.map((product: ProductType) => (
+                {products.map((product: ProductT) => (
                     <ProductCard key={product.id} {...product} />
                 ))}
             </div>
